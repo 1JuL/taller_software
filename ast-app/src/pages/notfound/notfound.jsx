8d2168fe257b871532e30b820987462a1e0cbad4
1JuL <diegodiax0122@gmail.com>
2025-01-25 16:39:30 -0500

    Changed 404 page style

--- ast-app/src/pages/notfound/notfound.jsx
+++ ast-app/src/pages/notfound/notfound.jsx
@@ -5,14 +5,16 @@ const NotFound = () => {
     const navigate = useNavigate();
     return (
         <>
-            <h1 className="text-2xl font-bold mb-4 text-white">Not found</h1>
-            <button
-                type="button"
-                className="btn btn-light w-100 mt-3 mb-3"
-                onClick={() => navigate('/')}
-            >
-                Volver al Inicio
-            </button>
+            <div className="min-vh-100 d-flex flex-column justify-content-center align-items-center bg-dark text-white">
+                <h1 className="text-2xl font-bold mb-4">Not found</h1>
+                <button
+                    type="button"
+                    className="btn btn-light w-30 mt-3 mb-3"
+                    onClick={() => navigate('/')}
+                >
+                    Volver al Inicio
+                </button>
+            </div>
         </>
     )
 }
