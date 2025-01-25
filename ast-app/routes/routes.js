f70469537f22f2e5d2200fe3657f7ccceba6b5b5
1JuL <diegodiax0122@gmail.com>
2025-01-24 21:03:41 -0500

    Added Bootstrap

---
+++ ast-app/routes/routes.js
@@ -0,0 +1,26 @@
+export const ROUTES = {
+  ROOT: {
+    path: "/",
+    label: "home",
+  },
+  OVERVIEW: {
+    path: "/overview",
+    label: "overview",
+  },
+  LOGIN: {
+    path: "/login",
+    label: "login",
+  },
+  PROFILE: {
+    path: "/profile",
+    label: "profile",
+  },
+  HOME: {
+    path: "/home",
+    label: "start",
+  },
+  ERROR_404: {
+    path: "/404",
+    label: "not found",
+  },
+};
